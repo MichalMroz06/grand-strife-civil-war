bd067274fa11e5c65c2e3dbdc1ffd18f16436554
MichalMroz06 <michalmroz2006@gmail.com>
2026-09-12 11:38:27 +0200

    feat(#GSCW-011): set GAME_VERSION variable to const

--- common/debug/debug_info.gd
+++ common/debug/debug_info.gd
@@ -3,13 +3,14 @@ extends CanvasLayer
 @onready var version_lbl: Label = $container/debug_info/vertical_container/version_lbl
 @onready var fps_lbl: Label = $container/debug_info/vertical_container/fps_lbl
 
-@export var game_version: String = "0.1.1-dev"
 @export var update_interval: float = 0.1
+
+const GAME_VERSION: String = "0.1.1-dev"
 
 var _time_passed: float = 0.0
 
 func _ready() -> void:
-	version_lbl.text = "Version: " + game_version
+	version_lbl.text = "Version: " + GAME_VERSION
 
 func _process(delta: float) -> void:
 	_time_passed += delta
